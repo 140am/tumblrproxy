c366ae4c8abc4e58417c60d6b15d8dff15e9fdba
Manuel Kreutz <mk@140.am>
2013-08-30 18:26:19 -0400

    add a `date_format` template filter to make it easy to pretty print tumblr dates

---
+++ tumblrproxy/lib/template_filter.py
@@ -0,0 +1,15 @@
+import datetime
+
+def date_format(date_s, format=None):
+    if not format:  # default formating
+        format = '%m/%d/%Y'
+    if not date_s: return '-'
+    if isinstance(date_s, str) or isinstance(date_s, unicode):  # parse a string
+        try:
+            date_obj = datetime.datetime.strptime(date_s, '%Y-%m-%d %H:%M:%S %Z')
+        except ValueError: return
+        date_obj = date_obj.strftime(format)
+    else:  # parse a date object
+        date_obj = date_s
+
+    return date_obj
